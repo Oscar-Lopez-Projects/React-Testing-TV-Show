4b83aa9a7c222d0864b422c3f0876122c6612528
Oscar-Lopez-Projects <d1lopeztv@gmail.com>
2020-06-04 19:56:52 -0700

    moved fetching data into a new folder

---
+++ src/api/fetchShow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+
+//moved the fetch data from app to aoi folder.
+  export const fetchShow = () => {
+     return axios
+     .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
+     .then(data => {
+         console.log(data);
+         return data;
+     })
+     .catch(error => {
+         console.error("error fetching data from api, err: ", error.message);
+     });
+ };
